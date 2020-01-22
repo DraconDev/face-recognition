2ae3f1abbf318c9d55130976adad27a8647e8fac
unknown <dracsharp@gmail.com>
2020-01-22 15:21:28 +0000

    signin form added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import particleOptions from "./components/particlesjs-config.json";
 // import { useSelector } from "react-redux";
 import Clarifai from "clarifai";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
 const app = new Clarifai.App({
   apiKey: "0917a244585b40baba3c9b5ba97575de"
@@ -21,24 +23,57 @@ function App() {
 
   const [input, setInput] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [faceBox, setFaceBox] = useState({});
+  const [route, setRoute] = useState("signin");
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   // INPUT
   const onInputChange = event => {
     setInput(event.target.value);
-    console.log(input);
+    // console.log(input);
+  };
+
+  // CALCULATE FACE LOCATION
+  const calculateFaceLocation = data => {
+    // console.log(data, "data");
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  // Display Box Around Faces
+  const displayFaceBox = boxData => {
+    // console.
+    setFaceBox({ ...boxData });
+    console.log(faceBox, boxData, "test");
+  };
+
+  // Route
+  const onRouteChange = newRoute => {
+    // setRoute(newRoute);
+    if (newRoute === "signout") {
+      setIsSignedIn(false);
+    } else if (newRoute === "home") {
+      setIsSignedIn(true);
+    }
+    setRoute(newRoute);
   };
 
   // USEEFFECT TEST
-  useEffect(() => {
-    // console.log("object");
-  }, []);
+  useEffect(() => {}, []);
 
   //SUBMIT API
-
   const onButtonSubmit = () => {
     setImageUrl(input);
-    // console.log(input, "test");
-    // console.log(imageUrl, "input");
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
@@ -48,31 +83,38 @@ function App() {
       )
       .then(
         function(response) {
-          console.log(
-            response.outputs[0].data.regions[0].region_info.bounding_box
-          );
+          displayFaceBox(calculateFaceLocation(response));
         },
         function(err) {
           // there was an error
         }
-      );
+      )
+      .catch(err => console.log(err));
   };
-
   return (
-    <div className="App">
-      <Particles
-        className="particles"
-        // params={particleOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={onInputChange}
-        onButtonSubmit={onButtonSubmit}
-      />
-      <FaceRecognition imageUrl={imageUrl} />
-    </div>
+    console.log(route, "route"),
+    (
+      <div className="App">
+        <Particles className="particles" params={particleOptions} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={onInputChange}
+              onButtonSubmit={onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} faceBox={faceBox} />
+          </div>
+        ) : route === "signin" ? (
+          <SignIn onRouteChange={onRouteChange} />
+        ) : (
+          <Register onRouteChange={onRouteChange} />
+        )}
+      </div>
+    )
   );
 }
 // <Rank/> <FaceRecognition/>
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,14 +1,23 @@
 import React from "react";
-// import "./FaceRecognition.css";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl }) => {
-  // console.log(imageUrl, "facerecog");
-  return (
-    <div className="center">
-      <div className="absolute mt2"></div>
-      <img src={imageUrl} alt="Face" width="500px" height="auto" />
-    </div>
-  );
+const FaceRecognition = ({imageUrl, faceBox}) => {
+    // console.log(faceBox, "facerecog");
+    return (
+        <div className="center ma">
+            <div className="absolute mt2">
+                <img id="inputimage" src={imageUrl} alt="Face" width="500px" height="auto"/>
+                <div
+                    className="bounding-box"
+                    style={{
+                    top: faceBox.topRow,
+                    right: faceBox.rightCol,
+                    bottom: faceBox.bottomRow,
+                    left: faceBox.leftCol
+                }}></div>
+            </div>
+        </div>
+    );
 };
 
 export default FaceRecognition;
